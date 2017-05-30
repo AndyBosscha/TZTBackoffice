b6d86e19d397f4e5416fd957c81db1297ec2772e
jeremybosscha <jeremybosscha@gmail.com>
2017-05-30 19:58:14 +0200

    Added packagemodel
    Added packagemodel

---
+++ src/tztbackoffice/Models/PackageModel.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tztbackoffice.Models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Andy
+ */
+public class PackageModel {
+    private int idPackage;
+    private int idCustomer;
+    private Date signupDateTime;
+    private double weight;
+    private double height;
+    private double length;
+    private double width;
+    private Date pickUpDate;
+    private Date deliveryDate;
+
+    public int getIdPackage() {
+        return idPackage;
+    }
+
+    public void setIdPackage(int idPackage) {
+        this.idPackage = idPackage;
+    }
+
+    public int getIdCustomer() {
+        return idCustomer;
+    }
+
+    public void setIdCustomer(int idCustomer) {
+        this.idCustomer = idCustomer;
+    }
+
+    public Date getSignupDateTime() {
+        return signupDateTime;
+    }
+
+    public void setSignupDateTime(Date signupDateTime) {
+        this.signupDateTime = signupDateTime;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public Date getPickUpDate() {
+        return pickUpDate;
+    }
+
+    public void setPickUpDate(Date pickUpDate) {
+        this.pickUpDate = pickUpDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+}
