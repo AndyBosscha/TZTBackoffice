5c6704e584d5d23b2f9859149989ff909cd5a5cb
jeremybosscha <jeremybosscha@gmail.com>
2017-05-30 21:09:31 +0200

    Changed layout of main screen
    Changed layout of main screen

--- src/tztbackoffice/Views/MainScreenTZT.java
+++ src/tztbackoffice/Views/MainScreenTZT.java
@@ -5,11 +5,15 @@
  */
 package tztbackoffice.Views;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -42,49 +46,86 @@ public class MainScreenTZT extends JFrame implements ActionListener {
         setTitle("TZT Backoffice");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+        setLayout(new FlowLayout());
         mainScreen = new JPanel();
         topBar = new JPanel();
         topBar.setPreferredSize(new Dimension(this.getWidth(), 50));
         topBar.setLayout(new FlowLayout());
-        //mainScreen.setLayout(new GridLayout(1, 8, 5, 5));
+        mainScreen.setLayout(new GridLayout(1, 1, 5, 5));
         nameLabel = new JLabel("Naam");
-        nameLabel.setPreferredSize(new Dimension(100, 40));
         topBar.add(nameLabel);
         nameTextfield = new JTextField();
+        nameTextfield.setPreferredSize(new Dimension(100, 30));
         topBar.add(nameTextfield);
                 
         medewerkerNrLabel = new JLabel("Medewerkernr");
         topBar.add(medewerkerNrLabel);
         medewerkerNrTextfield = new JTextField();
+        medewerkerNrTextfield.setPreferredSize(new Dimension(100, 30));
         topBar.add(medewerkerNrTextfield);
         
         woonplaatsLabel = new JLabel("Woonplaats");
         topBar.add(woonplaatsLabel);
         woonplaatsTextfield = new JTextField();
+        woonplaatsTextfield.setPreferredSize(new Dimension(100, 30));
         topBar.add(woonplaatsTextfield);
         
         statusLabel = new JLabel("Status");
         topBar.add(statusLabel);
-        statusCombobox = new JComboBox();
+        String[] comboBoxList = {"Nieuw", "Actief", "Inactief"};
+        statusCombobox = new JComboBox(comboBoxList);
+        statusCombobox.setPreferredSize(new Dimension(100, 30));
+        
         topBar.add(statusCombobox);
-        
-        
         
         add(topBar);
         
-//        
-//        String[] columns = new String[]{
-//            "Id", "Name", "Hourly Rate", "Part Time"
-//        };
-//        Object[][] data = new Object[][]{
-//            {1, "John", 40.0, false},
-//            {2, "Rambo", 70.0, false},
-//            {3, "Zorro", 60.0, true},};
-//        JTable table = new JTable(data, columns);
-//        mainScreen.add(new JScrollPane(table));
-//
-//        add(mainScreen);
+        
+        String[] columns = new String[]{
+            "Naam", "Medewerkernr", "Woonplaats", "Geboortedatum", "Datum in dienst", "Aantal opdrachten aangenomen", "Aantal pakketten bezorgd", "Status"
+        };
+        Object[][] data = new Object[][]{
+            {"John", 4230, "Sexbierum", "12/11/1977", "12/03/2015", 532, 54, "Actief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 33, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 533, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 43, 444, "Nieuw"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Actief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 66664, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Nieuw"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Actief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Nieuw"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Actief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 4342, 444, "Inactief"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Nieuw"},
+            {"Rambo", 7220, "Rochel", "02/03/1953", "01/08/2011", 782, 444, "Inactief"}
+        };
+        JTable table = new JTable(data, columns){
+            public boolean isCellEditable(int row, int column){
+                return false;
+           }
+        };
+        
+        table.addMouseListener(new MouseAdapter() {
+        public void mousePressed(MouseEvent me) {
+                JTable table =(JTable) me.getSource();
+                Point p = me.getPoint();
+                int row = table.rowAtPoint(p);
+                if (me.getClickCount() == 2) {
+                    System.out.println("2");
+                }
+            }
+        });
+        
+        mainScreen.setPreferredSize(new Dimension(1150, 875));
+        mainScreen.setBackground(Color.red);
+        mainScreen.add(new JScrollPane(table));
+        add(mainScreen);
 
         setVisible(true);
     }
@@ -92,5 +133,5 @@ public class MainScreenTZT extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
     }
-
+    
 }
