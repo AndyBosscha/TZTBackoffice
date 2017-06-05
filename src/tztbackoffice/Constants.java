58a7b4f0fad3b5b3ad0a037aff75dc702de46f08
tztbackoffice <s1110703@student.windesheim.nl>
2017-06-05 12:35:07 +0200

    added sql connection data
    added sql connection data

--- src/tztbackoffice/Constants.java
+++ src/tztbackoffice/Constants.java
@@ -10,18 +10,29 @@ package tztbackoffice;
  * @author Andy
  */
 public class Constants {
-    
+
     public static final String tempPassword = "beheerder";
     public static final String tempUsername = "beheerder";
     public static final String loginFailureMessage = "Logingegevens incorrect. Probeer het nogmaals";
-    
+
     //DB
     public static final String DB_DRIVER = "jdbc:mysql://";
-    public static final String DB_PW = "tzt_admin";
-    public static final String DB_USN = "tzt_admin";
-    public static final String DB_ADDRESS = "localhost";
-    public static final String DB_PORT = "8889";
+    public static final String DB_PW = "4KtP2Kdo!";
+    public static final String DB_USN = "tztdb";
+    public static final String DB_ADDRESS = "127.142.113";
+    public static final String DB_PORT = "3306";
     public static final String DB_NAME = "tzt";
-    
-    
+
+//
+//    public static final String DB_PW = "tzt_admin";
+//    public static final String DB_USN = "tzt_admin";
+//    public static final String DB_ADDRESS = "localhost";
+//    public static final String DB_PORT = "8889";
+//    public static final String DB_NAME = "tzt";    
 }
+//// database settings
+//define('DB_TYPE', 'mysql');
+//define('DB_HOST', '80.127.142.113');
+//define('DB_NAME', 'tzt');
+//define('DB_USER', 'tztdb');
+//define('DB_PASS', '4KtP2Kdo!');
